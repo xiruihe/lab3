8e27312cfda9d7867724e2b956e996ba92d43c8e
Xirui He <xih173@ucsd.edu>
2019-01-24 10:49:21 -0800

    add effect

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,6 +5,25 @@ $(document).ready(function() {
 	initializePage();
 })
 
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+    var containingProject = $(this).closest(".project");
+	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	
+}
+    // var containingProject = $(this).closest(".project");
+    // var description = $(containingProject).find(".project-description");
+    // if (description.length == 0) {
+    //    $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    // } else {
+    //    description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    // }
+
+
 /*
  * Function that is called when the document is ready.
  */
@@ -12,7 +31,11 @@ function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
-
+		$(".jumbotron p").addClass("active");
+		$("#testjs").text("Please wait...");
+	$("a.thumbnail").click(projectClick);
+	$("#testjs").toggleClass("active");
+	
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
